811ec4109caf435e32c6dd0559f306ff154f06f8
xiexielucks <xyqlucks@163.com>
2019-01-29 19:29:38 +0800

    idea commit

--- src/main/java/com/test/testgit/util/Test.java
+++ src/main/java/com/test/testgit/util/Test.java
@@ -3,7 +3,7 @@ package com.test.testgit.util;
 public class Test {
     public static void main(String[] args) {
         System.out.println("我要提交了。。。。。");
-        System.out.println("在来。。。");
+        System.out.println("在来.....");
         System.out.println("为什么github没有");
         System.out.println("llllllllllllll");
         System.out.println("33333333333333");
