4ce8fc4eaa7833faa095b5955b7b06a2ae69a650
xcodinglife <xcodinglife@qq.com>
2019-01-29 20:48:18 +0800

    pig

--- src/main/java/com/test/testgit/util/HelloWord.java
+++ src/main/java/com/test/testgit/util/HelloWord.java
@@ -2,13 +2,6 @@ package com.test.testgit.util;
 
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("HelloWord.....");
-        System.out.println("123123123.....");
-        System.out.println("111111111111.....");
-        System.out.println("222222222222.....");
-        System.out.println("333333333333.....");
-        System.out.println("444444444444.....");
-        System.out.println("555555555555.....");
-        System.out.println("我之前按的是Git->Commit File");
+        System.out.println("你是猪嘞");
     }
 }
