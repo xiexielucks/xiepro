90c873be3be43776a7cbc65c585a15bca329d37e
xiexielucks <xyqlucks@163.com>
2019-01-29 19:47:55 +0800

    idea commit

--- src/main/java/com/test/testgit/util/HelloWord.java
+++ src/main/java/com/test/testgit/util/HelloWord.java
@@ -3,9 +3,6 @@ package com.test.testgit.util;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord.....");
-        System.out.println("我试一试");
-        System.out.println("哈哈哈我知道了");
-        System.out.println("lllllllllll");
-        System.out.println("33333333333333");
+     
     }
 }
