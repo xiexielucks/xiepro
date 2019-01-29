6ec511f578af28980a6a26b59980cdad9cdcb8a5
xiexielucks <xyqlucks@163.com>
2019-01-29 18:07:19 +0800

    idea commit

--- src/main/java/com/test/testgit/util/Test.java
+++ src/main/java/com/test/testgit/util/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("我要提交了。。。。。");
         System.out.println("在来。。。");
+        System.out.println("为什么github没有");
     }
 }
