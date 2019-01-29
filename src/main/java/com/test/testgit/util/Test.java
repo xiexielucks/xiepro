e1903bf90f26e60497e117a514600b36f7ef993e
xiexielucks <xyqlucks@163.com>
2019-01-29 17:59:45 +0800

    idea commit

---
+++ src/main/java/com/test/testgit/util/Test.java
@@ -0,0 +1,8 @@
+package com.test.testgit.util;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我要提交了。。。。。");
+        System.out.println("在来。。。");
+    }
+}
