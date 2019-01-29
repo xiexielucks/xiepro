6ff8d647a86ce4f77dcbca6dadfa31952e01cf44
xiexielucks <xyqlucks@163.com>
2019-01-29 19:57:50 +0800

    idea commit

--- src/main/java/com/test/testgit/util/HelloWord.java
+++ src/main/java/com/test/testgit/util/HelloWord.java
@@ -4,7 +4,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord.....");
         System.out.println("123123123.....");
-        System.out.println("5555555555555.....");
         System.out.println("222222222222.....");
         System.out.println("我之前按的是Git->Commit File");
     }
