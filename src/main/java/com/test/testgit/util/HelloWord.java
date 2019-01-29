e0a938efc501169910d9e4190361addbc769fc95
xcodinglife <xcodinglife@qq.com>
2019-01-29 20:52:08 +0800

    pig

--- src/main/java/com/test/testgit/util/HelloWord.java
+++ src/main/java/com/test/testgit/util/HelloWord.java
@@ -2,6 +2,6 @@ package com.test.testgit.util;
 
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("你是猪嘞");
+        System.out.println("你是猪嘞~");
     }
 }
